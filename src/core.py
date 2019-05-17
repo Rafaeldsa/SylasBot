ab7b6174e43a2fec0298f5e2bbb012a8db3ae168
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2019-05-17 14:18:01 -0300

    function talk

--- src/core.py
+++ src/core.py
@@ -1,35 +1,35 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 import time
 from conf.settings import TELEGRAM_TOKEN
-
+#coding: utf-8
 def start(bot, update):
-	i = 0
-	while i < 2:
-		
-		response_message = "Salve"
-		bot.send_message(
-			chat_id=update.message.chat_id,
-			text=response_message
-			
-		)
-		time.sleep(120)
-		i += 1
+	response_message = "Salve"
+	bot.send_message(
+		chat_id=update.message.chat_id,
+		text=response_message
+)
+
+def talk(bot, update, args):
+	response_message = "Ola, meu nome eh Sylas, e o seu?"
+	bot.send_message(
+		chat_id=update.message.chat_id,
+		text=response_message + agrs[0]
+	)
 		
 
 def main():
-    updater = Updater(token=TELEGRAM_TOKEN)
-
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(
-        CommandHandler('start', start)
-)
-
-    updater.start_polling()
-
-    updater.idle()
+	updater = Updater(token=TELEGRAM_TOKEN)
+	dispatcher = updater.dispatcher
+	dispatcher.add_handler(
+		CommandHandler('start', start)
+	)
+	dispatcher.add_handler(
+		CommandHandler('talk',talk)
+	)
+	updater.start_polling()
+	updater.idle()
 
 
 if __name__ == '__main__':
-    print("press CTRL + C to cancel.")
+    print("No ar...")
 main()

89fbd6c3bdf8e9225e0c442ec9fca4ec534fe270
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2019-05-16 23:42:53 -0300

    funcao sleep

--- src/core.py
+++ src/core.py
@@ -1,13 +1,20 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-
+import time
 from conf.settings import TELEGRAM_TOKEN
 
 def start(bot, update):
-    response_message = "Salve"
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response_message
-    )
+	i = 0
+	while i < 2:
+		
+		response_message = "Salve"
+		bot.send_message(
+			chat_id=update.message.chat_id,
+			text=response_message
+			
+		)
+		time.sleep(120)
+		i += 1
+		
 
 def main():
     updater = Updater(token=TELEGRAM_TOKEN)
